e3731304800e860faa5bbf840208ef19356518eb
MulyonoPutra <mulyonoputra1994@gmail.com>
2022-01-08 12:01:00 +0700

    6 files modified

--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -1,12 +1,12 @@
-import { Component, OnInit, Input } from "@angular/core";
+import { Component, OnInit, Input, AfterViewInit } from '@angular/core';
 
 @Component({
-  selector: "app-child",
-  templateUrl: "./child.component.html",
-  styleUrls: ["./child.component.css"]
+  selector: 'app-child',
+  templateUrl: './child.component.html',
+  styleUrls: ['./child.component.css'],
 })
 export class ChildComponent implements OnInit {
-  @Input() childMessage: string;
+  @Input() childMessage: {};
 
   constructor() {}
 
--- src/app/parent/parent.component.ts
+++ src/app/parent/parent.component.ts
@@ -1,13 +1,21 @@
-import { Component, OnInit } from "@angular/core"; 
+import { Component, OnInit } from '@angular/core';
 @Component({
-  selector: "app-parent",
-  templateUrl: "./parent.component.html",
-  styleUrls: ["./parent.component.css"]
+  selector: 'app-parent',
+  templateUrl: './parent.component.html',
+  styleUrls: ['./parent.component.css'],
 })
 export class ParentComponent implements OnInit {
-  
-  parentMessage = "This is a message from parent"
-  constructor() { }
+
+  person = {};
+  constructor() {}
 
   ngOnInit(): void {}
+
+  send(){
+    this.person = {
+      name: 'Mulyono Putra',
+      phone: '081289900099'
+    }
+    console.log(this.person)
+  }
 }
